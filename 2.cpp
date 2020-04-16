33724a137a07fa09d9218fb4bd675df13cf38c39
tanya011 <57870762+tanya011@users.noreply.github.com>
2020-04-16 16:40:32 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+struct time{
+    int left, right;
+};
+bool f(time a1, time a2){
+    if(a1.left > a2.left)
+        swap(a1, a2);
+    if (a1.right > a2.left)
+        return true;
+    else
+        return false;
+}
 int main(){
-    int k = 1;
-    int last = 0;
     int n;
     cin >> n;
-    int l[n], r[n];
+    vector <time> lessons;
+    vector<vector <time>> classrooms;
     for (int i = 0; i < n; i++){
-        cin >> l[i] >> r[i];
+        time t;
+        cin >> t.left >> t.right;
+        lessons.push_back(t);
     }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (r[j] > r[j + 1])
-                swap(r[j], r[j+1]);
-                swap(l[j], l[j+1]);
-            }
-        }
-    for (int i = 1; i < n; i++){
-        if (l[i] >= r[last]){
-            k++;
-            last = i;
-        }
-    }
-    cout << k;
+    for (int i = 0; i < n; i++) {
+			int num_room = -1;
+			for (int j = 0; j < classrooms.size(); j++) {
+				bool flag = false;
+				for (int q = 0; q < classrooms[j].size(); ++q) {
+					if (f(classrooms[j][q], lessons[i])) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag == false) {
+					num_room = j;
+					break;
+				}
+			}
+			if (num_room == -1) {
+				vector <time> new_room(1, lessons[i]);
+				classrooms.push_back(new_room);
+			}
+			else
+				classrooms[num_room].push_back(lessons[i]);
+		}
+
+    cout << classrooms.size();
 }
