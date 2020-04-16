d18979b50998666666eb0f470f6102bb4b1d2fe9
tanya011 <57870762+tanya011@users.noreply.github.com>
2020-04-16 14:25:12 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -9,7 +9,13 @@ int main(){
     for (int i = 0; i < n; i++){
         cin >> l[i] >> r[i];
     }
-
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (r[j] > r[j + 1])
+                swap(r[j], r[j+1]);
+                swap(l[j], l[j+1]);
+            }
+        }
     for (int i = 1; i < n; i++){
         if (l[i] >= r[last]){
             k++;
